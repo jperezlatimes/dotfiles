288361bd679f832912cc5c1cd402d934d8199c66
James Perez <jdperez04@gmail.com>
2019-04-02 16:47:12 -0700

    Spaceship theme (#2)
    * adding spaeship theme support
    
    * Create .zshrc-base
    
    Base file that zshrc-theme files will inherit from
    
    * Create .zshrc-powerlevel9k
    
    powerlevel9k specific .zshrc settings
    
    * Update install.sh
    
    * Delete .zshrc
    
    * add spaceship theme file
    
    * Update .zshrc-spaceship

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Create backup directory if it doesn't exist
 if [ ! -d ~/.dotfiles/backup ]; then
   mkdir ~/.dotfiles/backup
@@ -6,6 +8,20 @@ fi
 # Backup old dotfiles
 mv ~/.zshrc ~/.dotfiles/backup/.zshrc || echo "No .zshrc file to move"
 mv ~/.vimrc ~/.dotfiles/backup/.vimrc || echo "No .vimrc file to move"
+
+# Write zshrc file with theme link
+sudo rm -f .zshrc && touch .zshrc
+echo "export source ~/.zshrc-base" >> .zshrc
+
+# If theme is specified, add it
+if [ "$0" != "" ]; then
+  if [ ! -f .zshrc-$0 ]; then
+    echo "ZSH theme $0 not supported, using default. Supported themes: spaceship, powerlevel9k"
+  else
+    echo "Initializing ZSH profile with $0 theme."
+    echo "export source ~/.zshrc-$0" >> .zshrc
+  fi
+fi
 
 # Create symlinks to new dotfiles
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
