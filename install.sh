c6dad37404b3076968be3298da2ff16b98b812b1
James Perez <james.perez@latimes.com>
2019-04-02 16:00:20 -0700

    adding spaeship theme support

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+THEME='spaceship'
+
+# Take first argument as theme name
+if [ "$0" != "" ]; then
+  if [ ! -f .zshrc-$0 ]; then
+    echo "ZSH theme $0 not supported please select from: spaceship, powerlevel9k"
+  else
+    echo "Initializing ZSH profile with $0 theme."
+    THEME=$0
+  fi
+fi
+
 # Create backup directory if it doesn't exist
 if [ ! -d ~/.dotfiles/backup ]; then
   mkdir ~/.dotfiles/backup
@@ -9,7 +23,7 @@ mv ~/.vimrc ~/.dotfiles/backup/.vimrc || echo "No .vimrc file to move"
 
 # Create symlinks to new dotfiles
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.zshrc-$THEME ~/.zshrc
 
 # Load the new zsh config
 source ~/.zshrc
